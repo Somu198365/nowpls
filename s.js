b92e0936216bfae1bef8c45d15c10d5b23b5b4f5
Somu198365 <73246101+Somu198365@users.noreply.github.com>
2020-12-20 20:44:55 +0530

    Add files via upload

---
+++ s.js
@@ -0,0 +1,22 @@
+player1_name= localStorage.getItem("player1_name");
+player2_name= localStorage.getItem("player2_name");
+player1_score=0;
+player2_score=0;
+document.getElementById("player1_name").innerHTML=player1_name+" : ";
+document.getElementById("player2_name").innerHTML=player2_name+" : ";
+document.getElementById("player1_score").innerHTML=player1_score;
+document.getElementById("player2_score").innerHTML=player2_score;
+document.getElementById("q").innerHTML= "Question turn-"+player1_name;
+document.getElementById("a").innerHTML= "Answer turn-"+player2_name;
+function send(){
+number1=document.getElementById("word").value;
+number2=document.getElementById("word2").value;
+actual_answer=parseInt(number1)*paradeInt(number2);
+question_number="<h4>"+number1+"X"+number2+"</h4>";
+input_box="<br>Answer : <input type='text' id='input_check_box'>";
+check_button="<br><br><button class='btn btn-danger' onclick='check'>Check</button>";
+row=question_number+input_box+check_button;
+document.getElementById("output").innerHTML=row;
+document.getElementById("word").value="";
+document.getElementById("word2").value="";
+}
